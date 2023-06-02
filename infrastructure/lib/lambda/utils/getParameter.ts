b02cce7e8edc8f1cbbfef75e1433cae6df1c9e57
Shashank Shrivastava <shashrv@amazon.com>
2023-06-01 21:56:29 -0700

    Changing parameter store to secretsmanager

--- infrastructure/lib/lambda/utils/getParameter.ts
+++ infrastructure/lib/lambda/utils/getParameter.ts
@@ -2,23 +2,22 @@
 // SPDX-License-Identifier: MIT-0
 
 import AWS = require('aws-sdk');
-const ssm = new AWS.SSM({
-    apiVersion: '2014-11-06'
+const ssm = new AWS.SecretsManager({
+    region: process.env.AWS_REGION
 });
 
 export const getParamFromSSM = async (path: string) => {
     try {
         const query = {
-            Name: path,
-            WithDecryption: true,
+            SecretId: path
         };
-        const getParameterResult = await ssm.getParameter(query).promise();
+        const getParameterResult = await ssm.getSecretValue(query).promise();
 
         if (getParameterResult === undefined) {
             console.error("Unable to getParameter with this query: " + query);
             return "No Data"
         } else {
-            return getParameterResult.Parameter?.Value;
+            return getParameterResult.SecretString;
         }
     } catch (error) {
         console.error(`*** Error: ${error}`);
